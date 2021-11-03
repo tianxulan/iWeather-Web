7f17f4222464ace23c064731bb286ce074017d58
Xiu <kevin.lantx@gmail.com>
2021-11-02 22:11:04 -0700

    implement input validation

--- frontend/weather-search/src/app/search-form/search-form.component.ts
+++ frontend/weather-search/src/app/search-form/search-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators, FormBuilder, ValidatorFn, AbstractControl, ValidationErrors} from '@angular/forms';
+const nonWhitespaceRegExp: RegExp = new RegExp("\\S");
 
 @Component({
   selector: 'search-form',
@@ -6,11 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-form.component.css']
 })
 export class SearchFormComponent implements OnInit {
-  inputStreet = '';
-
-  constructor() { }
-
+  searchForm!: FormGroup;
+  spaceOnlyPattern = ".*[^ ].*";
+  constructor(private fb: FormBuilder) { 
+    
+    
+    this.searchForm = this.fb.group({
+      inputStreet: ['',Validators.required],
+      inputCity: ['', Validators.required ]
+      inputSubmit:['']
+   });
+  
+  }
+  
+  forbiddenNameValidator(nameRe: RegExp): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const forbidden = nameRe.test(control.value);
+      return forbidden ? {forbiddenName: {value: control.value}} : null;
+    };
+  }
   ngOnInit(): void {
+    
+    
   }
 
 }
+
