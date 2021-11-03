8b474a945dc35145eec09d2471d6f192115d897d
Xiu <kevin.lantx@gmail.com>
2021-11-02 18:22:54 -0700

    search form cotent and style built

--- frontend/weather-search/src/app/app.module.ts
+++ frontend/weather-search/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { SearchFormComponent } from './search-form/search-form.component';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/weather-search/src/app/search-form/search-form.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'search-form',
+  templateUrl: './search-form.component.html',
+  styleUrls: ['./search-form.component.css']
+})
+export class SearchFormComponent implements OnInit {
+  inputStreet = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
