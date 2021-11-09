e67e6d5bef9985332ec3974a9a591f921e3705ce
Xiu <kevin.lantx@gmail.com>
2021-11-09 13:31:28 -0800

    daily temp partily developed, no gradient fill color for now

--- frontend/weather-search/src/app/daily-temp/daily-temp.component.ts
+++ frontend/weather-search/src/app/daily-temp/daily-temp.component.ts
@@ -14,7 +14,12 @@ ExportData(Highcharts);
 
 const Accessibility = require('highcharts/modules/accessibility');
 Accessibility(Highcharts);
-
+interface extendTooltip extends Highcharts.Tooltip {
+  crosshairs: boolean,
+  shared: boolean,
+  valueSuffix: string,
+  xDateFormat: string
+}
 @Component({
   selector: 'app-daily-temp',
   templateUrl: './daily-temp.component.html',
@@ -23,53 +28,64 @@ Accessibility(Highcharts);
 export class DailyTempComponent implements OnInit {
 
   daily:any = null;
-  temperatureRangeData= [];
+  temperatureRangeData= [{
+    name:'temp',
+    data: []
+  }];
   Highcharts: typeof Highcharts = Highcharts; // required
-  chartOptions: Highcharts.Options = {
-    chart: {
-      type: 'arearange',
-      height: '500'
-  },
-  title: {
-      text: 'Temperature Ranges(Min, Max)'
-  },
-  xAxis: {
-      type: "datetime",
-      accessibility:
-      {
-          rangeDescription: 'Range: Jan 1st 2017 to Dec 31 2017.'
-      }
-  },
-  yAxis: {
-      title: {
-          text: null
-      }
-  },
-  tooltip: {
-      shared: true,
-      valueSuffix: ' °F',
-      xDateFormat: '%A, %b %e'
-  },
-  plotOptions: {
-      series: {
-         
-      }
-  },
-  legend: {
-      enabled: false
-  },
-  series: []
-  } ; // required
-  constructor(private _dailyDataTransferService: DailyDataTransferService) { }
+  chartOptions: Highcharts.Options ;
+    
+  constructor(private _dailyDataTransferService: DailyDataTransferService) {
+    
+   }
+
 
   ngOnInit(): void {
     this._dailyDataTransferService.currentDailyData.subscribe(daily => {
       this.daily = daily.data.timelines[0].intervals;
       daily.data.timelines[0].intervals.forEach(element => {
-        this.temperatureRangeData.push([new Date(element.startTime).valueOf(), element.values.temperatureMin, element.values.temperatureMax])
+        this.temperatureRangeData[0].data.push([new Date(element.startTime).valueOf(), element.values.temperatureMin, element.values.temperatureMax])
       });
       console.log(this.temperatureRangeData);
-      this.chartOptions.series = this.temperatureRangeData;
+      this.chartOptions = {
+        chart: {
+          type: 'arearange',
+          height: '400'
+      },
+      title: {
+          text: 'Temperature Ranges(Min, Max)'
+      },
+      xAxis: {
+          type: "datetime",
+          accessibility:
+          {
+              rangeDescription: 'Range: Jan 1st 2017 to Dec 31 2017.'
+          }
+      },
+      yAxis: {
+          title: {
+              text: null
+          }
+      },
+      tooltip: {
+        crosshairs: true,
+                shared: true,
+                valueSuffix: ' °F',
+                xDateFormat: '%A, %b %e'
+          
+      } as extendTooltip,
+      
+      legend: {
+          enabled: false
+      },
+      series: [{
+        name: "temperatres",
+        type:'arearange',
+        data: this.temperatureRangeData[0].data
+      }
+      ]
+    }
+      
     });
     
   }
