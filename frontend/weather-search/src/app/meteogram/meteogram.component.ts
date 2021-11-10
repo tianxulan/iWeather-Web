61228faa8ef51e00f36b8b0703105483ea684bba
Xiu <kevin.lantx@gmail.com>
2021-11-09 17:12:20 -0800

    add highcharts package for metegram component

--- frontend/weather-search/src/app/meteogram/meteogram.component.ts
+++ frontend/weather-search/src/app/meteogram/meteogram.component.ts
@@ -1,6 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { HourlyDataTransferService } from '../hourly-data-transfer.service';
+import * as Highcharts from 'highcharts';
+import { DailyDataTransferService } from '../daily-data-transfer.service';
 
+declare var require: any;
+const More = require('highcharts/highcharts-more');
+More(Highcharts);
+
+const Exporting = require('highcharts/modules/exporting');
+Exporting(Highcharts);
+
+const ExportData = require('highcharts/modules/export-data');
+ExportData(Highcharts);
+
+const Accessibility = require('highcharts/modules/accessibility');
+Accessibility(Highcharts);
 @Component({
   selector: 'app-meteogram',
   templateUrl: './meteogram.component.html',
