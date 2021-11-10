ba6f532be9b7139c475a0e5fb2b80e2335222639
Xiu <kevin.lantx@gmail.com>
2021-11-09 16:27:34 -0800

    set queryExample for prod to false

--- frontend/weather-search/src/environments/environment.prod.ts
+++ frontend/weather-search/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: "https://weather-node-330706.wn.r.appspot.com",
-  queryExample:true,
+  queryExample: false,
   googleApiKey: "AIzaSyBy8dLihEmKyaXkWKyn5DGUz1tNMC-8Dp8",
   autoCompleteDelayTime:10,
 };
