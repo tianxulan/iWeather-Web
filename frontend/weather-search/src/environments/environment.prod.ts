ea3bef68a76232969ef9eee203f5410d5e94a8bc
Xiu <kevin.lantx@gmail.com>
2021-12-26 19:18:19 -0800

    remove enviorment files from source tree to protect api key

--- frontend/weather-search/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: "https://weather-node-330706.wn.r.appspot.com",
-  queryExample: false,
-  googleApiKey: "AIzaSyBy8dLihEmKyaXkWKyn5DGUz1tNMC-8Dp8",
-  autoCompleteDelayTime:10,
-};
